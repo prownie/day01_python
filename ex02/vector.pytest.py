c2010bb06b668d72de77e35fa6cb0eeb2f8fd5fb
rpichon <pichon.robin1@gmail.com>
2020-05-26 22:38:50 +0200

    add testing file ex02

--- ex02/vector.pytest.py
+++ ex02/vector.pytest.py
@@ -10,18 +10,23 @@ class Vector:
                 while i < val:
                     self.values.append(float(i))
                     i += 1
+                self.size = i
         elif isinstance(val, list):
+            self.size = 0
             for i in val:
                 if not isinstance(i, int) and not isinstance(i, float):
                     exit("Only accepts ints and floats")
                 self.values.append(float(i))
+                self.size += 1
         elif isinstance(val, tuple):
             if (len(val) != 2 or (type(val[0]) != float)
                or (type(val[1]) != float)):
                 exit("Only accepts range with 2 floats, with nb1 < nb2")
             i = val[0]
+            self.size = 0
             while int(i) < int(val[1]):
                 self.values.append(i)
+                self.size += 1
                 i += 1
         else:
             exit("Accepted format : Vector(float), Vector((float,float)),\
@@ -40,7 +45,7 @@ class Vector:
                 i += 1
             return(rep)
         else:
-            print("Can't add, different vector sizes")
+            print("Can't add, different vector sizes",end='')
 
     def __radd__(self, val):
         rep = []
@@ -55,7 +60,7 @@ class Vector:
                 i += 1
             return(rep)
         else:
-            print("Can't add, different vector sizes")
+            print("Can't add, different vector sizes",end='')
 
     def __sub__(self, val):
         rep = []
@@ -70,7 +75,7 @@ class Vector:
                 i += 1
             return(rep)
         else:
-            print("Can't substract, different vector sizes")
+            print("Can't substract, different vector sizes",end='')
 
     def __rsub__(self, val):
         rep = []
@@ -85,7 +90,7 @@ class Vector:
                 i += 1
             return(rep)
         else:
-            print("Can't substract, different vector sizes")
+            print("Can't substract, different vector sizes",end='')
 
     def __truediv__(self, val):
         rep = []
@@ -94,7 +99,7 @@ class Vector:
                 rep.append(i / val)
             return(rep)
         else:
-            print("Can only divid vector by a scalar not null (int/float)")
+            print("Can only divid vector by a scalar not null (int/float)",end='')
 
     def __rtruediv__(self, val):
         rep = []
@@ -106,7 +111,7 @@ class Vector:
                 rep.append(val / i)
             return(rep)
         else:
-            print("Can only divid vector by a scalar not null (int/float)")
+            print("Can only divid vector by a scalar not null (int/float)",end='')
 
     def __mul__(self, val):
         rep = []
@@ -122,7 +127,7 @@ class Vector:
                 i += 1
             return rep
         else:
-            print("Can only multiply vector of the same size, or by a scalar")
+            print("Can only multiply vector of the same size, or by a scalar",end='')
 
     def __rmul__(self, val):
         rep = []
@@ -138,10 +143,80 @@ class Vector:
                 i += 1
             return rep
         else:
-            print("Can only multiply vector of the same size, or by a scalar")
+            print("Can only multiply vector of the same size, or by a scalar",end='')
 
+    def __str__(self):
+        toprint = "Vecteur Values :["
+        i = 0
+        while i < self.size - 1:
+            toprint += "{}, ".format(self.values[i])
+            i += 1
+        toprint += "{}]".format(self.values[i])
+        toprint += "\nVecteur Size = {}".format(self.size)
+        return(toprint)
 
+    def __repr__(self):
+        toret = "Vector(["
+        i = 0
+        while i < self.size - 1:
+            toret += "{}, ".format(self.values[i])
+            i += 1
+        toret += "{}])".format(self.values[i])
+        return(toret)
+
+print("----------Differents vectors init----------\n")
+
+print("--- init with list of floats ---")
+print("vec1 = Vector([2.0, 1.0, 0.0, 5.0, 10.0])")
 vec1 = Vector([2.0, 1.0, 0.0, 5.0, 10])
+print("print vec1 = ", vec1.values, "| size =", vec1.size, "\n")
+
+print("--- init with range of vectors ---")
+print("vec2 = Vector((2.0,7.0))")
 vec2 = Vector((2.0, 7.0))
-print(vec1.values)
-print(5 * vec)
+print("print vec2 = ", vec2.values, "| size =", vec2.size, "\n")
+
+print("--- init with a size ---")
+print("vec3 = Vector(10)")
+vec3 = Vector(10)
+print("print vec3 = ", vec3.values, "| size =", vec3.size, "\n")
+
+print("----------Add tests----------\n")
+print("vec1 + vec2 =",vec1 + vec2)
+print("5 + vec2    =",5 + vec2)
+print("vec2 + 5    =",vec2 - 5)
+print("vec2 + vec3 = ",end='')
+print(vec2 + vec3)
+
+
+print("\n----------Sub tests----------\n")
+print("vec1 - vec2 =",vec1 - vec2)
+print("5 - vec2    =",5 - vec2)
+print("vec2 - 5    =",vec2 - 5)
+print("vec2 - vec3 = ",end='')
+print(vec2 - vec3)
+
+print("\n----------mul tests----------\n")
+print("vec1 * vec2 =",vec1 * vec2)
+print("5 * vec2    =",5 * vec2)
+print("vec2 * 5    =",vec2 * 5)
+print("vec2 * vec3 = ",end='')
+print(vec2 * vec3)
+
+print("\n----------div tests----------\n")
+print("vec1 / vec2 =",end='')
+print(vec1 / vec2)
+print("5 / vec2    =",5 / vec2)
+print("vec2 / 5    =",vec2 / 5)
+print("vec2 / vec3 = ",end='')
+print(vec2 / vec3)
+
+
+print("\n----------str tests----------\n")
+print("print(vec1) :")
+print(vec1)
+
+print("\n----------repr tests----------\n")
+print("vectest=eval(repr(vec1)):")
+vectest=eval(repr(vec1))
+print("vectest=\n",vectest) 
